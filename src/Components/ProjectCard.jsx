260fb7e1dd3d5d94ae6b5e783a615635c9b62ec1
Allison <119072380+AllisonNB@users.noreply.github.com>
2025-02-17 14:04:17 -0500

    updated projects

--- src/Components/ProjectCard.jsx
+++ src/Components/ProjectCard.jsx
@@ -1,21 +1,29 @@
-import { Link } from "react-scroll";
-import './ProjectCard.css';
+import "./ProjectCard.css";
 
-
-export default function ProjectCard({ title, image, description, github, live }) {
-    return (
-        <figure>
-            <div className='frame'>
-                <img src={image} alt="" />
-            </div>
-            <figcaption className='cap'>
-                <h2>{title}</h2>
-                <p>{description}</p>
-                <div className='links'>
-                    <a className='link' href={live} target="_blank">Live</a>
-                    <a className='link' href={github} target="_blank">Github</a>
-                </div>
-            </figcaption>
-        </figure>
-    )
+export default function ProjectCard({
+  title,
+  image,
+  description,
+  github,
+  live,
+}) {
+  return (
+    <figure>
+      <div className="frame">
+        <img src={image} alt="" />
+      </div>
+      <figcaption className="cap">
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <div className="links">
+          <a className="link" href={live} target="_blank">
+            Live
+          </a>
+          <a className="link" href={github} target="_blank">
+            Github
+          </a>
+        </div>
+      </figcaption>
+    </figure>
+  );
 }
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,40 +1,59 @@
-import './Projects.css';
+import "./Projects.css";
 import ProjectCard from "./ProjectCard";
-import PuzzleSwap from '/assets/PuzzleSwap.jpg';
-import TvTuneTrivia from '/assets/TvTuneTrivia.jpg';
-import TentsnTrails from '/assets/Tents-Trails.jpg'
-
+import PuzzleSwap from "/assets/PuzzleSwap.jpg";
+import TrendyThreads from "/assets/TrendyThreads.jpg";
+import TentsnTrails from "/assets/Tents-Trails.jpg";
+import MuseumOfCandy from "/assets/MuseumOfCandy.jpg";
+import TvTuneTrivia from "/assets/TvTuneTrivia.jpg";
 
 export default function Projects() {
-    return (
-        <div className='grid'>
-            <div className='project1'>
-                <ProjectCard
-                    title='Campground'
-                    image={TentsnTrails}
-                    description='A full-stack review site that allows users to add reviews of different campgrounds.'
-                    github='https://github.com/AllisonNB/Tents-Trails/tree/main'
-                    live='https://tentsandtrails.netlify.app/'
-                ></ProjectCard>
-            </div>
-            <div className='project2'>
-                <ProjectCard
-                    title='TV Tune Trivia'
-                    image={TvTuneTrivia}
-                    description={`Using Spotify's API, users guess TV shows based on an audio clip of the theme song.`}
-                    github='https://github.com/AllisonNB/TvTuneTrivia'
-                    live='https://tvtunetrivia.netlify.app'
-                ></ProjectCard>
-            </div>
-            <div className='project3'>
-                <ProjectCard
-                    title='PuzzleSwap'
-                    image={PuzzleSwap}
-                    description='Using Pragmatic DnD, users drag pieces to assemble the correct image.'
-                    github='https://github.com/AllisonNB/PuzzleSwap'
-                    live='https://puzzleswap.netlify.app'
-                ></ProjectCard>
-            </div>
-        </div>
-    )
+  return (
+    <div className="grid">
+      <div className="project1">
+        <ProjectCard
+          title="Tents & Trails"
+          image={TentsnTrails}
+          description="A full-stack review site that allows users to add reviews of different campgrounds."
+          github="https://github.com/AllisonNB/Tents-Trails/tree/main"
+          live="https://tentsandtrails.netlify.app/"
+        ></ProjectCard>
+      </div>
+      <div className="project2">
+        <ProjectCard
+          title="Trendy Threads"
+          image={TrendyThreads}
+          description={`A fabric store where users can filter and select fabrics to add to their cart`}
+          github="https://github.com/AllisonNB/FabricStore"
+          live="https://trendythreadz.netlify.app/"
+        ></ProjectCard>
+      </div>
+      <div className="project3">
+        <ProjectCard
+          title="PuzzleSwap"
+          image={PuzzleSwap}
+          description="Using Pragmatic DnD, users drag pieces to assemble the correct image."
+          github="https://github.com/AllisonNB/PuzzleSwap"
+          live="https://puzzleswap.netlify.app"
+        ></ProjectCard>
+      </div>
+      <div>
+        <ProjectCard
+          title="Tv Tune Trivia"
+          image={TvTuneTrivia}
+          description="A trivia game where users guess the TV show from the theme song."
+          github="https://github.com/AllisonNB/TvTuneTrivia"
+          live="https://tvtunetrivia.netlify.app/"
+        ></ProjectCard>
+      </div>
+      <div>
+        <ProjectCard
+          title="Museum of Candy"
+          image={MuseumOfCandy}
+          description="My first website with responsive design with HTML & CSS. This is where I started!"
+          github="https://github.com/AllisonNB/MuseumOfCandy"
+          live="https://museofcandy.netlify.app/"
+        ></ProjectCard>
+      </div>
+    </div>
+  );
 }
